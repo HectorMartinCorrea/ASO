7ca84fcfde90e83f3f5a85f3d71a70443749d328
Héctor Martin <hectormartincorrea207@gmail.com>
2026-09-22 13:41:56 +0200

    Ejercicios a mitad

---
+++ bash/sesion1_repaso/sesion2_funciones/calculadora.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+menu(){
+
+    #Imprime
+   echo ---Calculadora---
+   echo 1 - Suma
+   echo 2 - Resta
+   echo 3 - Multi  plicacion  
+   echo 4 - Division
+   echo 5 - Salir
+}
+
+menu
+
+
+while $opcion -ne 5; do
+read -p "Introduzca una de las anteriores opciones: " opcion
+
+if [[$opcion -eq 5]]; then
+    exit
+fi
+read -p "Introduzca un numero: " num1
+read -p "Introduzca otro numero: " num2
+case $opcion in
+    1) res=$(($num1 + $num2)) ;;
+    2) res=$(($num1-$num2)) ;;
+    3) res=$(($num1*$num2)) ;;
+    4) res=$(($num1/$num2)) ;;         
+    *) ;;    
+esac
+
+echo "Este es el resultado de la operación $res" 
+menu
+done
